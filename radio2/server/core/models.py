3dbba06f42cfa8d14e523b6c196f8d7842138cb5
dashnash <dashiell.nash@gmail.com>
2015-01-26 21:55:30 -0800

    Adding ORM relationships (TESTS BROKEN)
    Adding ORM relationships to commands, users, and devices.
    Attempting to properly use setup/teardown functions

--- radio2/server/core/models.py
+++ radio2/server/core/models.py
@@ -3,7 +3,8 @@ Created on Jan 26, 2015
 
 @author: dashnash
 '''
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from radio2.server.data import Base
 
 class Device(Base):
@@ -11,18 +12,24 @@ class Device(Base):
     Stores device information
     '''
     __tablename__ = 'devices'
-    
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
-    test_field = Column(Integer)
+    commands = relationship("Command", backref=backref("device"))
+    
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    device_id = Column(String(50))
+    display_name = Column(String(50))
+    commands = relationship("Command", backref=backref("user"))
 
 class Command(Base):
     __tablename__ = 'commands'
     id = Column(Integer, primary_key=True)
     op = Column(String(255))
-    #TODO link to user
-    #TODO link to device
-    
+    user_id = Column(Integer, ForeignKey('users.id'))
+    device_id = Column(Integer, ForeignKey('devices.id'))
 
 class Station(Base):
     __tablename__ = 'stations'
@@ -33,13 +40,7 @@ class Station(Base):
     skip_op = Column(String(255))
     # TODO image? static url?
     
-class Users(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    device_id = Column(String(50))
-    display_name = Column(String(50))
-    
-class ClientApps(Base):
+class ClientApp(Base):
     __tablename__ = 'client_apps'
     id = Column(Integer, primary_key=True)
     version_string = Column(String(10))
--- radio2/server/tests/data_test.py
+++ radio2/server/tests/data_test.py
@@ -7,19 +7,42 @@ import unittest
 from radio2.server.core import connect
 
 from radio2.server.data import Base, Session
-from radio2.server.core.models import Device
+from radio2.server.core.models import Device, User, Command
+from sqlalchemy.schema import CreateSchema, DropSchema
 
 test_config_path = 'configs/unittests.json'
 
+connection = connect.Connection(test_config_path)
+
 class Test(unittest.TestCase):
     
-    def testSchemaCreate(self):
-        connection = connect.Connection(test_config_path)
-        Base.metadata.create_all(connection.engine)
-    
-    def testDevicesAddandDelete(self):
-        connection = connect.Connection(test_config_path)
+#     def setUp(self):
+#         connection = connect.Connection(test_config_path)
+#         connection.engine.execute(DropSchema('unittests'))
+#         connection.engine.execute(CreateSchema('unittests'))
+# 
+#         Session.configure(bind=connection.engine)        
+#         Base.metadata.create_all(connection.engine)
+
+    @classmethod
+    def setUpClass(cls):
         Session.configure(bind=connection.engine)
+        connection.engine.execute(DropSchema('unittests'))
+        connection.engine.execute(CreateSchema('unittests'))
+        Base.metadata.create_all(connection.engine)
+        super(Test, cls).setUpClass()
+        
+    @classmethod
+    def tearDownClass(cls):
+        super(Test, cls).tearDownClass()
+    
+    
+#     def testAAASchemaCreate(self):
+#         Base.metadata.create_all(connection.engine)
+      
+    def testDevicesAddandDelete(self):
+#         connection = connect.Connection(test_config_path)
+#         Session.configure(bind=connection.engine)
         test_session = Session()
         test_session.add_all([ \
             Device(name='dev1'), \
@@ -28,6 +51,21 @@ class Test(unittest.TestCase):
         
         test_session.commit()
         
+    def testCommandAddandDelete(self):
+#         connection = connect.Connection(test_config_path)
+#         Session.configure(bind=connection.engine)
+        test_session = Session()
+        
+        dev = Device(name='testLinkDevice')
+        usr = User(display_name='FunkyFresh', device_id='x10422_junk')
+        comm = Command(op='Play pandora station \'Funk\'', user=usr, device=dev)
+        
+        test_session.add(comm)
+        
+        test_session.commit()
+        
+        self.assertTrue(len(usr.commands) == 1)
+        self.assertTrue(comm.device == dev)
         
         
         
